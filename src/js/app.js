5ff016de864b0288befc68e461ac7b63379780f0
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-11-23 13:03:29 -0500

    HTML CSS template

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,24 @@
-console.log('app worked')
+function showAddCardForm(columnId) {
+    var column = document.getElementById(columnId);
+    var form = document.createElement('div');
+    form.className = 'add-card-form';
+    form.innerHTML = `
+        <input type="text" id="newCardInput" placeholder="Enter card title">
+        <button onclick="addCard('${columnId}')">Add Card</button>
+    `;
+    column.appendChild(form);
+    document.getElementById('newCardInput').focus();
+}
+
+function addCard(columnId) {
+    var column = document.getElementById(columnId);
+    var cardTitle = document.getElementById('newCardInput').value;
+    if (cardTitle.trim() !== '') {
+        var card = document.createElement('div');
+        card.className = 'card';
+        card.innerHTML = cardTitle;
+        column.insertBefore(card, column.lastChild);
+        document.getElementById('newCardInput').value = '';
+        column.removeChild(column.lastChild); // Remove the add card form
+    }
+}
