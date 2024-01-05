597a8b20117873cf1d42fdaacb56f9bd45b3d14f
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2024-01-05 17:18:41 -0500

    fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,108 @@
 document.addEventListener('DOMContentLoaded', function () {
     restoreCards();
 
+    var draggingElement = null;
+    var draggingProection = null;
+    var shiftX = null;
+    var shiftY = null;
+       
+    const onMouseDown = (evt) => {
+        const target = evt.target;
+        evt.preventDefault();
+        delElement.style.display = 'none'
+
+		if (target.classList.contains('card')) {
+			shiftX = evt.offsetX;
+			shiftY = evt.offsetY;
+            const { width, height } = window.getComputedStyle(target);
+			draggingElement = target.cloneNode(true);
+            document.body.appendChild(draggingElement);
+			draggingElement.style = `
+		 		left: ${evt.pageX - shiftX - 1}px;
+		 		top: ${evt.pageY - shiftY - 1}px;
+                position: absolute;
+                width: ${width};
+                height: ${height};
+			`
+            const proection = elementProection(target);
+            target.replaceWith(proection);
+		}
+	}
+
+	const onMouseUp = () => {
+		if (draggingElement) {
+			draggingProection.replaceWith(draggingElement);
+            draggingElement.style = {};
+            document.querySelector('.proection').remove();
+            draggingElement = null;
+            draggingProection = null;
+            saveCards();
+		}
+	}
+
+    const onMouseMove = (evt) => {
+		if (draggingElement) {
+			const { pageX, pageY } = evt;
+			const { width, height } = window.getComputedStyle(draggingElement);
+			draggingElement.style = `
+				position: absolute;
+		 		left: ${pageX - shiftX - 1}px;
+		 		top: ${pageY - shiftY - 1}px;
+		 		pointer-events: none;
+				width: ${width};
+				height: ${height};
+                display: block;
+			`
+            proectionAct(evt);
+		}
+	}
+    
+    function elementProection (element) {
+        const proection = document.createElement('div');
+        proection.classList = 'proection';
+        const { width, height } = window.getComputedStyle(element);
+        proection.style.cssText = `
+        width: ${width};
+        height: ${height};
+        margin: 10px 0;
+        `
+        return proection
+    }
+
+    function proectionAct (evt) {
+		const target = evt.target;
+		const proection = draggingProection;
+		if (
+			target.classList.contains("card") &&
+			!target.classList.contains("proection")
+		) {
+			const { y, height } = target.getBoundingClientRect();
+			const appendPosition = y + height / 2 > evt.clientY
+				? "beforebegin"
+				: "afterend";
+
+			if (!proection) {
+				draggingProection = elementProection(target);
+			} else {
+				proection.remove();
+				target.insertAdjacentElement(appendPosition, proection);
+			}
+		}
+	}
+
+    document.body.addEventListener('mousedown', onMouseDown);
+    document.body.addEventListener('mouseup', onMouseUp);
+    document.body.addEventListener('mousemove', onMouseMove);
+
+
+
+    // Создает крестик для удаления
     const delElement = document.createElement('div');
     delElement.textContent = '✖';
     delElement.className = 'cross';
     document.body.appendChild(delElement);
 
+    // Функционал удаления карточки при нажатии на крестик
     delElement.addEventListener('click', function (event) {
         var x = event.clientX;
         var y = event.clientY;
@@ -15,34 +112,7 @@ document.addEventListener('DOMContentLoaded', function () {
         saveCards();
     });
 
-    var draggedElement = null;
-
-    var columns = document.querySelectorAll('.column');
-    columns.forEach(function (column) {
-        column.addEventListener('dragenter', function (event) {
-            event.preventDefault();
-            delElement.style.display = 'none';
-        });
-
-        column.addEventListener('dragover', function (event) {
-            event.preventDefault();
-        });
-
-        column.addEventListener('dragleave', function () {
-            delElement.style.display = 'block';
-        });
-
-        column.addEventListener('drop', function () {
-            if (draggedElement) {
-                var cardsContainer = column.querySelector('.cards-container');
-                cardsContainer.appendChild(draggedElement);
-                delElement.style.display = 'none';
-                saveCards();
-                draggedElement = null;
-            }
-        });
-    });
-
+    // Функционал кнопок ADD
     var addCardButtons = document.querySelectorAll('.add-card-btn');
     addCardButtons.forEach(function (button) {
         button.addEventListener('click', function () {
@@ -57,85 +127,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function showAddCardForm(columnId) {
         var columnContainer = document.getElementById(columnId);
-    
         if (columnContainer) {
             var cardsContainer = columnContainer.querySelector('.cards-container');
             const newCard = document.createElement('div');
             newCard.className = 'card';
             newCard.textContent = 'NEW EMPTY CARD';
             cardsContainer.appendChild(newCard);
-    
             setupCard(newCard);
         }
     }
     
     function setupCard(card) {
-        setupCardDragEvents(card);
         setupCardEditEvents(card);
         setupCardDelElement(card);
     }
     
     function setupCardDelElement(card) {
         card.addEventListener('mouseover', () => {
-            const crossRect = card.getBoundingClientRect();
-            delElement.style.top = crossRect.top + 2 + 'px';
-            delElement.style.left = crossRect.x + crossRect.width - 25 + 'px';
-            delElement.style.display = 'block';
+            if (draggingElement == null) {
+                const crossRect = card.getBoundingClientRect();
+                delElement.style.top = crossRect.top + 2 + 'px';
+                delElement.style.left = crossRect.x + crossRect.width - 25 + 'px';
+                delElement.style.display = 'block';    
+            }
         });
     
         card.addEventListener('mouseout', () => {
             delElement.style.display = 'none';
-        });
-    }
-    
-    function setupCardDragEvents(card) {
-        card.draggable = true;
-    
-        card.addEventListener('dragstart', function (event) {
-            draggedElement = event.target;
-            event.dataTransfer.setData('text/plain', '');
-            card.style.cursor = 'grabbing';
-        });
-    
-        card.addEventListener('dragenter', function (event) {
-            event.preventDefault();
-            delElement.style.display = 'none';
-        });
-    
-        card.addEventListener('dragover', function (event) {
-            event.preventDefault();
-            const previousSibling = card.previousElementSibling;
-            if (previousSibling == null || !previousSibling.classList.contains("empty-placeholder")) {
-                addPlaceholder(card);
-            }
-        });
-    
-        card.addEventListener('dragleave', function () {
-            delElement.style.display = 'block';
-            const placeholder = document.querySelector('.empty-placeholder');
-            placeholder.remove();
-        });
-    
-        card.addEventListener('drop', function () {
-            if (draggedElement) {
-                var column = this.closest('.column');
-                if (column) {
-                    var cardsContainer = column.querySelector('.cards-container');
-                    cardsContainer.insertBefore(draggedElement, this);
-                }
-            }
-            const placeholder = document.querySelector('.empty-placeholder');
-            placeholder.remove();
-            delElement.style.display = 'none';
-            saveCards();
-            draggedElement = null;
-        });
-    
-        card.addEventListener('dragend', function () {
-            card.style.cursor = 'grab';
-            setTimeout(() => {
-                card.style.cursor = '';
-            }, 0);
         });
     }
     
@@ -192,12 +210,5 @@ document.addEventListener('DOMContentLoaded', function () {
                 });
             }
         }
-    }
-
-    function addPlaceholder(card) {
-        const emptyPlaceholder = document.createElement('div');
-        emptyPlaceholder.className = 'empty-placeholder';
-        const cardsContainer = card.parentElement;
-        cardsContainer.insertBefore(emptyPlaceholder, card);
     }
 });
